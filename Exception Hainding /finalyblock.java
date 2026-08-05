e521a499671a93bd054b7ef14f52e87d5e25570a
Md Shahriar Rahman <shuvorahman932@gmail.com>
2026-08-05 17:08:40 +0600

    Implement try-catch-finally for division by zero

--- Exception Hainding /finalyblock.java
+++ Exception Hainding /finalyblock.java
@@ -1,1 +1,16 @@
-
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Start");
+        try {
+            int a=10;
+            int b=0;
+            System.out.println(a / b);
+        } catch(ArithmeticException e){
+            System.out.println("Can't devided by Zero");
+        }
+        finally {
+            System.out.println("Finished");
+        }
+        System.out.println("END");
+    }
+}
