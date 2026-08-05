120a411a139b2b13add2bc947439ada60f49926a
Md Shahriar Rahman <shuvorahman932@gmail.com>
2026-08-05 11:06:42 +0600

    Implement exception handling for division by zero
    Added exception handling for division by zero in Basic class.

--- Exception Hainding /basic.java
+++ Exception Hainding /basic.java
@@ -9,5 +9,20 @@ public class Basic {
         System.out.println("end");
     }
 }
-*output will be:Exception in thread "main" java.lang.ArithmeticException: / by zero
-	at exceptionhandling.Basic.main(Basic.java:8)
+/*output will be:Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at exceptionhandling.Basic.main(Basic.java:8)**/
+
+// with exception handling :
+public class Basic{
+    public static void main(String[] args){
+        System.out.println("Start");
+        try{
+            int x=10;
+            int y=0;
+            System.out.println(x/y);
+        } catch (ArithmeticException e){
+            System.out.println("Cannot divide by the zero.");
+        }
+        System.out.println("End");
+    }
+}
