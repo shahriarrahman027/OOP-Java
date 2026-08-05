67a500ca811aa708b1acf91e41da37e487cb92a9
Md Shahriar Rahman <shuvorahman932@gmail.com>
2026-08-05 10:57:33 +0600

    Refactor Main class to Basic with exception handling
    Updated the class structure and added exception handling for division by zero.

--- Exception Hainding /basic.java
+++ Exception Hainding /basic.java
@@ -1,5 +1,13 @@
-public class Main{
-    public static void main(String[] args){
-        system.out.println("hello");
+package exceptionhandling;
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        int x=10;
+        int  y=0;
+        System.out.println(x/y);
+        System.out.println("end");
     }
 }
+*output will be:Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at exceptionhandling.Basic.main(Basic.java:8)
